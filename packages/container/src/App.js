b4100a9ab7a5b11de62898a2d451517027e0b7d4
Gaurav Joshi <gaurav.joshi@publicissapient.com>
2021-12-06 10:33:40 +0530

    first commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+// import { mount } from 'marketing/MarketingApp';
+// console.log(mount);
+
+export default () => {
+    return (<MarketingApp />);
+};
