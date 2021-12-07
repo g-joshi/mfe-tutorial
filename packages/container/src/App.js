7bf05667e6cd34afbf2331dbd46982408d23e5e7
Gaurav Joshi <gaurav.joshi@publicissapient.com>
2021-12-07 12:42:43 +0530

    fixed css collisions

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
+import { BrowserRouter } from 'react-router-dom';
 import MarketingApp from './components/MarketingApp';
-// import { mount } from 'marketing/MarketingApp';
+import Header from './components/Header';
 // console.log(mount);
 
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+});
+
+
 export default () => {
-    return (<MarketingApp />);
+    return (
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </BrowserRouter>
+        </StylesProvider>
+    );
 };
